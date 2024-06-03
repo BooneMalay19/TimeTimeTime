18b5e9655541a08082b24e61f7dec48546195757
Pedro Brito de Souza <144711331+BooneMalay19@users.noreply.github.com>
2024-06-02 23:57:21 -0300

    Add files via upload

---
+++ script.js
@@ -0,0 +1,20 @@
+function updateTimer() {
+    const endTime = new Date('June 3, 2024 23:59:00').getTime();
+    const now = new Date().getTime();
+    const timeLeft = endTime - now;
+
+    const days = Math.floor(timeLeft / (1000 * 60 * 60 * 24));
+    const hours = Math.floor((timeLeft % (1000 * 60 * 60 * 24)) / (1000 * 60 * 60));
+    const minutes = Math.floor((timeLeft % (1000 * 60 * 60)) / (1000 * 60));
+    const seconds = Math.floor((timeLeft % (1000 * 60)) / 1000);
+
+    if (timeLeft < 0) {
+        document.getElementById('timer').innerHTML = "EXPIRED";
+        document.getElementById('redirectButton').style.display = 'block';
+    } else {
+        document.getElementById('timer').innerHTML = 
+            days + "d " + hours + "h " + minutes + "m " + seconds + "s ";
+    }
+}
+
+setInterval(updateTimer, 1000);
